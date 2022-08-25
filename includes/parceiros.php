037d175f85365fa47d3b539ffd22c57eb5aacdf0
tais1301 <tbc1301@gmail.com>
2022-08-24 21:44:33 -0300

    Initial commit

---
+++ includes/parceiros.php
@@ -0,0 +1,30 @@
+<div id="parceiros">
+    <div>
+        <p class="text-center font-weight-bold text-white parceiro">NOSSOS PARCEIROS</p>
+    </div>
+    <div id="carousel-controles" class="carousel slide" data-ride="carousel">
+
+        <div class="carousel-inner">
+        <div class="carousel-item active row">
+                <img src="logos_banco/safra.png" alt="Banco Safra" class="col-3 img-banco-safra">
+                <img src="logos_banco/pan.png" alt="Banco Pan" class="col-3 img-banco-pan">
+                <img src="logos_banco/banrisul.png" alt="Banco Banrisul" class="col-3 img-banco-banrisul">
+                <img src="logos_banco/C6.png" alt="C6 Bank" class="col-3 img-banco-c6">
+            </div>
+            <div class="carousel-item row"> 
+                <img src="logos_banco/cetelem.png" alt="Cetelem" class="col-2 img-banco-cetelem">
+                <img src="logos_banco/daycoval.png" alt="Daycoval" class="col-2 img-banco-daycoval">
+                <img src="logos_banco/mercantil.png" alt="mercantil" class="col-2 img-banco-mercantil">
+                <img src="logos_banco/facta.png" alt="facta" class="col-2 img-banco-facta">
+            </div>
+        </div>
+
+        <a href="#carousel-controles" class="carousel-control-prev" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a href="#carousel-controles" class="carousel-control-next" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </a>
+
+    </div>
+</div>
